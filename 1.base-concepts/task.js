e8dd6ffe784ae4cb27665d9f9b2f71299cc07335
Vasek56 <96530839+Vasek56@users.noreply.github.com>
2022-05-27 01:57:24 +0300

    Start write code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,25 @@
+"use strict"
+let discriminant;
+let x;
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  discriminant = (b** - 4*a*c);
+  if (discriminant < 0) {
+    x = "Корней нет"
+  }
+  if (discriminant == 0){
+    x = -b/(2*a);
+  }
+  if (discriminant > 0){
+    x = (-b + Math.sqrt(d) )/(2*a);
+    x = (-b - Math.sqrt(d) )/(2*a);
+  }
+
   return arr; // array
 }
+solveEquation (1, 2, 10)
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
