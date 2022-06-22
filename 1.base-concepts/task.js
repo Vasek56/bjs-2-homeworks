5dd4ffa3fd4195e3a91b33a84e45c5018901bf58
Vasek56 <96530839+Vasek56@users.noreply.github.com>
2022-06-22 23:34:40 +0300

    add new code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,24 +1,30 @@
 "use strict"
-let discriminant;
-let x;
+let d;
+let x, x1, x2, x3;
+let arr = [];
 
 function solveEquation(a, b, c) {
-  let arr;
-  discriminant = (b** - 4*a*c);
-  if (discriminant < 0) {
-    x = "Корней нет"
+  d = Math.pow(b, 2) - (4 * a * c)
+  if (d < 0) {
+    x = "Дискриминант";
+   arr[x] = "Корней нет";
+    
   }
-  if (discriminant == 0){
-    x = -b/(2*a);
+  if (d == 0){
+    x1 = -b/(2*a);
+    arr[x1] = "Корень равен";
   }
-  if (discriminant > 0){
-    x = (-b + Math.sqrt(d) )/(2*a);
-    x = (-b - Math.sqrt(d) )/(2*a);
+  if (d > 0){
+    x2 = (-b + Math.sqrt(d)) / (2 * a);
+    x3 = (-b - Math.sqrt(d)) / (2 * a);
+    arr[x2] = "Первый корень";
+    arr[x3] = "Второй корень";
   }
-
-  return arr; // array
+ return arr;
 }
-solveEquation (1, 2, 10)
+let result = solveEquation (1, 2, 1)
+console.log(result)
+console.log(d)
 
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
